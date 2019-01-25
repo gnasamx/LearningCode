de1f8375bc3f1f5a811173558e9e7226a53f318b
gnasamx <pawarg124@gmail.com>
2019-01-25 20:41:29 +0530

    :tada: Learn React useState hook

---
+++ modern-react/src/exercises/01.js
@@ -0,0 +1,25 @@
+// Counter: useState
+// 🐨 you'll need to add {useState} to this import statement
+import React, {useState} from 'react'
+
+// 💰 the `useState` hook allows you to use state
+// from within function components in react:
+// const [name, setName] = useState('Angela')
+
+function Counter() {
+  // 🐨 you'll call useState here to get count and setCount
+  // 🐨 render the count here and add an onClick handler that increments the count
+  const [count, setCount] = useState(0)
+  const increment = () => setCount(count + 1)
+  return <button onClick={increment}>{count}</button>
+}
+
+// Don't make changes to the Usage component. It's here to show you how your
+// component is intended to be used and is used in the tests.
+
+function Usage() {
+  return <Counter />
+}
+Usage.title = 'Counter: useState'
+
+export default Usage
---
+++ modern-react/src/exercises/02.js
@@ -0,0 +1,29 @@
+// Counter: custom hooks
+import React, {useState} from 'react'
+
+// 🐨 create a function here called useCounter
+// the "use" prefix is a convention, and not required.
+// don't overthink this. It's JavaScript :)
+// 💰 make sure to export it for the tests.
+
+function useCounter() {
+  const [count, setCount] = useState(0)
+  const incrementCount = () => setCount(count + 1)
+  return {count, incrementCount}
+}
+
+function Counter() {
+  // 🐨 move these two lines to your function and return what you need
+  const {count, incrementCount} = useCounter()
+  return <button onClick={incrementCount}>{count}</button>
+}
+
+// Don't make changes to the Usage component. It's here to show you how your
+// component is intended to be used and is used in the tests.
+
+function Usage() {
+  return <Counter />
+}
+Usage.title = 'Counter: custom hooks'
+
+export default Usage
