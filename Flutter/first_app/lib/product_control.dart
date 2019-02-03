cb5dc25c488123fd0331da3c42caec2fa50babc0
gnasamx <gnasamx@gmail.com>
2019-02-03 09:05:39 +0530

    :package: Learn widgets and flutter basics

---
+++ Flutter/first_app/lib/product_control.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductControl extends StatelessWidget {
+  final Function addProduct;
+  ProductControl(this.addProduct);
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+        child: Text('Add Product'),
+        onPressed: () {
+          addProduct('Sweets');
+        });
+  }
+}
