7d69ada904ee1d5bcef660e28822bf6b0b49e748
gnasamx <gnasamx@gmail.com>
2019-02-03 11:26:56 +0530

    :racing_car: Learn type checking, query vs mutation, arrays, objects, arguments

--- GraphQL/beginner-graphql/src/index.js
+++ GraphQL/beginner-graphql/src/index.js
@@ -1,14 +1,73 @@
 const { ApolloServer, gql } = require('apollo-server')
 
+/**
+ * Type Checking
+ * Query vs. Mutation
+ * Arrays
+ * Object
+ * Arguments
+ */
+
+/**
+ * Type Checking: Anything you put in your type definitions graphql is going to check the type of it or
+ * at least it is going to try casting it to that type.
+ */
+
 const typeDefs = gql`
   type Query {
     hello: String!
+    user: User!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+  }
+
+  type Error {
+    field: String!
+    message: String!
+  }
+
+  type RegisterResponse {
+    errors: [Error]
+    user: User!
+  }
+
+  input UserInfo {
+    username: String!
+    password: String!
+    age: Int
+  }
+
+  type Mutation {
+    register(userInfo: UserInfo): RegisterResponse!
+    login(userInfo: UserInfo): Boolean!
   }
 `
 
 const resolvers = {
   Query: {
     hello: () => 'Hello World',
+    user: () => ({
+      id: 1,
+      username: 'Bob',
+    }),
+  },
+  Mutation: {
+    login: () => true,
+    register: () => ({
+      errors: [
+        {
+          field: 'username',
+          message: 'Bad',
+        },
+      ],
+      user: {
+        id: 1,
+        username: 'Bob',
+      },
+    }),
   },
 }
 
