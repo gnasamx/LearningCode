29a6982fc85f33bd097ab9993aa3d5625fcd02ca
gnasamx <gnasamx@gmail.com>
2019-02-05 22:54:04 +0530

    :truck: Add navigation

--- GraphQL/graphql-react-event-booking/frontend/src/App.js
+++ GraphQL/graphql-react-event-booking/frontend/src/App.js
@@ -3,17 +3,24 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 import AuthPage from './pages/Auth'
 import BookingsPage from './pages/Bookings'
 import EventPage from './pages/Events'
+import MainNavigation from './components/Navigation/MainNavigation'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect exact from="/" to="/auth" />
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/events" component={EventPage} />
-          <Route path="/bookings" component={BookingsPage} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect exact from="/" to="/auth" />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     )
   }
