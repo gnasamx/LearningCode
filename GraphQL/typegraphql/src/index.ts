8857ab0e31d9b406d75f91188fd60dbebbaca143
gnasamx <gnasamx@gmail.com>
2019-02-06 17:37:55 +0530

    :volcano: Add hello world example

--- GraphQL/typegraphql/src/index.ts
+++ GraphQL/typegraphql/src/index.ts
@@ -1,7 +1,30 @@
+import 'reflect-metadata'
 import { ApolloServer } from 'apollo-server-express'
 import * as Express from 'express'
-// import { buildSchema, Resolver } from 'type-graphql';
+import { buildSchema, Resolver, Query } from 'type-graphql'
 
-const main = async () => {}
+@Resolver()
+class HelloResolver {
+  @Query(() => String)
+  async hello() {
+    return 'Hello World'
+  }
+}
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+  })
+
+  const apolloServer = new ApolloServer({ schema })
+
+  const app = Express()
+
+  apolloServer.applyMiddleware({ app })
+
+  app.listen(4000, () => {
+    console.log('Server started on http://localhost:4000/graphql')
+  })
+}
 
 main()
