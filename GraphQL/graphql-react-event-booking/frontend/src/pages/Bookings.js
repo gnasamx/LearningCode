0f1a43c5149477f73b6a353a386d6d90a24e2c9d
gnasamx <gnasamx@gmail.com>
2019-02-14 22:19:26 +0530

    :construction: Delete bookings

--- GraphQL/graphql-react-event-booking/frontend/src/pages/Bookings.js
+++ GraphQL/graphql-react-event-booking/frontend/src/pages/Bookings.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import AuthContext from '../context/auth-context'
+import BookingList from '../components/Bookings/BookingList/BookingsList'
 
 class BookingsPage extends Component {
   state = {
@@ -58,16 +59,55 @@ class BookingsPage extends Component {
       })
   }
 
+  deleteBookingHandler = bookingId => {
+    this.setState({
+      isLoading: true,
+    })
+    const requestBody = {
+      query: `
+            mutation {
+              cancelBooking(bookingId: "${bookingId}") {
+                _id
+               title
+              }
+            }
+          `,
+    }
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.context.token,
+      },
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed!')
+        }
+        return res.json()
+      })
+      .then(resData => {
+        this.setState(prevState => {
+          const updatedBookings = prevState.bookings.filter(booking => {
+            return booking._id !== bookingId
+          })
+          return { bookings: updatedBookings, isLoading: false }
+        })
+      })
+      .catch(err => {
+        console.log(err)
+        this.setState({ isLoading: false })
+      })
+  }
+
   render() {
     return (
-      <u>
-        {this.state.bookings.map(booking => (
-          <li>
-            {booking.event.title} -
-            {new Date(booking.createdAt).toLocaleDateString()}
-          </li>
-        ))}
-      </u>
+      <BookingList
+        bookings={this.state.bookings}
+        onDelete={this.deleteBookingHandler}
+      />
     )
   }
 }
