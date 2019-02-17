21187f354c4551f8eb7f30c03afbdc634c0f257a
gnasamx <gnasamx@gmail.com>
2019-02-17 20:20:09 +0530

    :purple_heart: Complete user authentication with redirecting

--- Python/flask-blog/flaskblog/__init__.py
+++ Python/flask-blog/flaskblog/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -7,5 +9,10 @@ app.config['SECRET_KEY'] = '0cdea408de27ca946783888b36ca381a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
+# from flaskblog import routes
 from flaskblog import routes
