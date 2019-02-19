ef943f1bd2558419ff522fc61f8bfcfb675bfa2a
gnasamx <gnasamx@gmail.com>
2019-02-19 23:45:01 +0530

    :tickets: Use Blueprint and Restructure the whole app

--- Python/flask-blog/flaskblog/models.py
+++ Python/flask-blog/flaskblog/models.py
@@ -1,7 +1,8 @@
-from flaskblog import db, login_manager, app
 from datetime import datetime
-from flask_login import UserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
+from flaskblog import db, login_manager
+from flask_login import UserMixin
 
 
 @login_manager.user_loader
@@ -19,17 +20,17 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def get_reset_token(self, expires_sec=1800):
-        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
 
     @staticmethod
     def verify_reset_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
+        s = Serializer(current_app.config['SECRET_KEY'])
         try:
             user_id = s.loads(token)['user_id']
         except:
             return None
-        return user.query.get(user_id)
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
