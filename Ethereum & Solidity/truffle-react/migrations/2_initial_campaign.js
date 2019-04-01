4c3b18f89417558cd0fa887507bb2efe4d987011
gnasamx <gnasamx@gmail.com>
2019-04-01 09:30:41 +0530

    Truffle web3 now working with react

--- Ethereum & Solidity/truffle-react/migrations/2_initial_campaign.js
+++ Ethereum & Solidity/truffle-react/migrations/2_initial_campaign.js
@@ -1,4 +1,4 @@
 const Campaign = artifacts.require('Campaign')
 module.exports = function(deployer) {
-  deployer.deploy(Campaign, 10, '0xaa8598658d8c51d5375cb5a21fbfb94807af8c75' )
+  deployer.deploy(Campaign, 10, '0x2f867b7971871f8be6bd586d7fc6cc671001ff5f' )
 }
